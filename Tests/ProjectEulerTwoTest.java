38d9b3e439c44d0d4631c0c4576da00c21654801
Patrick Karjala <pkarjala@hawaii.edu>
2015-02-16 16:34:28 -1000

    New tests created.

---
+++ Tests/ProjectEulerTwoTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests the functionality of ProjectEulerTwo.
+ *
+ */
+public class ProjectEulerTwoTest {
+
+  /**
+   * Tests various input values for the calculateEvenFibonnaciSum function.
+   *
+   * @throws Exception An error message.
+   */
+  @Test
+  public void testCalculateEvenFibonacciSum() throws Exception {
+    assertEquals("Value of sum up to 35", 44, ProjectEulerTwo.calculateEvenFibonacciSum(35));
+    assertEquals("Value of sum up to 500", 188, ProjectEulerTwo.calculateEvenFibonacciSum(500));
+  }
+}
--- src/ProjectEulerTwo.java
+++ src/ProjectEulerTwo.java
@@ -1,20 +1,41 @@
-/* Created by patrick on 2/4/15.
-    */
+/**
+ *  Created by patrick on 2/4/15.
+ */
 public class ProjectEulerTwo {
+
+  /**
+   * Adds the sum of even Fibonnaci numbers up to 4 million.
+   * Then prints out the sum.
+   *
+   * @param args ignored.
+   */
   public static void main(String[] args) {
+
+    int sum = calculateEvenFibonacciSum(4000000);
+    System.out.println("Total is: " + sum);
+  }
+
+  /**
+   * Calculates the sum of all even numbers in the Fibonnaci sequence
+   * up to a certain max value.
+   *
+   * @param maxValue The maximum value to calculate up to.
+   * @return The sum of all even Fibonnaci numbers up to maxValue.
+   */
+  public static int calculateEvenFibonacciSum(int maxValue) {
+
     int previous = 1;
     int current = 2;
-    int next = 0;
+    int next;
     int sum = 0;
-    while (current < 4000000) {
+    while (current < maxValue) {
       if ((current % 2) == 0) {
         sum += current;
       }
       next = previous + current;
-//      System.out.println("Next is: " + current);
       previous = current;
       current = next;
     }
-    System.out.println("Total is: " + sum);
+    return sum;
   }
 }
